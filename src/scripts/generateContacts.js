b9ac7cf9de7a6b662131ae9a96add9760e27d253
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-03-21 12:44:17 +0200

    getAllContact

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,3 @@
-export const PATH_DB = '';
-
 import { createFakeContact } from '../utils/createFakeContact.js';
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
